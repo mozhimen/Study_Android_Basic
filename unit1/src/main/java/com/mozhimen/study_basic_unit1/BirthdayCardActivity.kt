b92d55299cee681189f64d172149d443fd1f5b2a
Kolin Zhao & Mozhimen <mozhimen@163.com>
2023-07-03 00:51:03 +0800

    0703

--- unit1/src/main/java/com/mozhimen/study_basic_unit1/BirthdayCardActivity.kt
+++ unit1/src/main/java/com/mozhimen/study_basic_unit1/BirthdayCardActivity.kt
@@ -2,15 +2,10 @@ package com.mozhimen.study_basic_unit1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class BirthdayCardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-    fun goBirthdayCard(view: View) {
-
+        setContentView(R.layout.activity_birthday_card)
     }
 }
