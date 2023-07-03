e3cefdfa6d186def003c45ae161705e84f414100
Kolin Zhao & Mozhimen <mozhimen@163.com>
2023-07-04 02:07:30 +0800

    0704

--- unit3/src/main/java/com/mozhimen/study_basic_unit3/MainActivity.kt
+++ unit3/src/main/java/com/mozhimen/study_basic_unit3/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.mozhimen.study_basic_unit3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.mozhimen.study_basic_unit3.dessert.DessertClickerActivity
+import com.mozhimen.study_basic_unit3.words.WordsActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goWords(view: View) {
+        startActivity(Intent(this, WordsActivity::class.java))
+    }
+
+    fun goDessertClickers(view: View) {
+        startActivity(Intent(this, DessertClickerActivity::class.java))
     }
 }
